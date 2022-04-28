7c62fe3f5f5b12bccc1bb1f9f74619abcb8b0770
clowninside <ivanfeduk17@gmail.com>
2022-04-28 16:30:33 +0300

    demo 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:asana_demo_2/pages/home.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  theme: ThemeData(
-    primarySwatch: Colors.red,
-  ),
-  home: Home(),
-));
+void initFirebase() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+}
+
+void main() {
+  initFirebase();
+  runApp(MaterialApp(
+    theme: ThemeData(
+      primarySwatch: Colors.red,
+    ),
+    home: Home(),
+  ));
+}
 
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -7,59 +9,86 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
+const STATE_TASK_DONE = "DONE";
+const STATE_TASK_ACTIVE = "ACTIVE";
+
+class Task {
+  String state = STATE_TASK_ACTIVE;
+  String userTodo = '';
+  String userPrice = '';
+
+  Task(this.userTodo, this.userPrice);
+}
+
 class _HomeState extends State<Home> {
-  String _userTodo = '';
-  String _userPrice = '';
-  List todoList = [];
-  Color _BasedColor = Colors.red;
+  List<Task> todoList = [];
+
+  void initFirebase() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+  }
 
   @override
   void initState() {
     super.initState();
-
-    todoList.addAll(['buy milk', 'suck cock']);
+    initFirebase();
+    todoList.add(Task('buy milk', "\$5.00"));
+    todoList.add(Task('rate app', "\$1.000.000"));
   }
 
   @override
   Widget build(BuildContext context) {
+    String _userTodo = '';
+    String _userPrice = '';
     return Scaffold(
       backgroundColor: Colors.white38,
       appBar: AppBar(
         title: Text('Task List'),
         centerTitle: true,
       ),
-      body: ListView.builder(
-          itemCount: todoList.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Dismissible(
-              key: Key(todoList[index]),
-              child: Card(
-                child: ListTile(
-                  title: Text(todoList[index]),
-                  trailing: IconButton(
-                    icon: Icon(
-                      Icons.check,
-                      color: _BasedColor,
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('items').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) return Text('no zapisi');
+          return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Dismissible(
+                  key: Key(snapshot.data!.docs[index].id),
+                  child: Card(
+                    child: ListTile(
+                      title: Text(snapshot.data!.docs[index].get('task') +
+                          " " +
+                          snapshot.data!.docs[index].get('price')),
+                      trailing: IconButton(
+                        icon: Icon(Icons.check,
+                            color: todoList[index].state == STATE_TASK_DONE
+                                ? Colors.green
+                                : Colors.red),
+                        onPressed: () {
+                          setState(() {
+                            if (todoList[index].state == STATE_TASK_DONE) {
+                              todoList[index].state = STATE_TASK_ACTIVE;
+                            } else {
+                              todoList[index].state = STATE_TASK_DONE;
+                            }
+                          });
+                        },
+                      ),
                     ),
-                    onPressed: () {
-                      setState(() {
-                        if (_BasedColor == Colors.red) {
-                          _BasedColor = Colors.green;
-                        } else {
-                          _BasedColor = Colors.red;
-                        }
-                      });
-                    },
                   ),
-                ),
-              ),
-              onDismissed: (direction) {
-                setState(() {
-                  todoList.removeAt(index);
-                });
-              },
-            );
-          }),
+                  onDismissed: (direction) {
+                    setState(() {
+                      FirebaseFirestore.instance
+                          .collection('items')
+                          .doc(snapshot.data!.docs[index].id)
+                          .delete();
+                    });
+                  },
+                );
+              });
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.red,
         onPressed: () {
@@ -96,10 +125,9 @@ class _HomeState extends State<Home> {
                   actions: [
                     ElevatedButton(
                         onPressed: () {
-                          setState(() {
-                            todoList.add(_userTodo);
-                            //todoList.add(_userPrice);
-                          });
+                          FirebaseFirestore.instance
+                              .collection('items')
+                              .add({'task': _userTodo, 'price': _userPrice});
 
                           Navigator.of(context).pop();
                         },
